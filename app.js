523f5bb2c073d170c1296957581911e6ceb90974
Sang Won Kim <sangwonkim@SangWon-MacBook-Air.local>
2023-06-28 12:26:05 +0900

    ADD : InitialSettings

--- app.js
+++ app.js
@@ -3,26 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
-const { DataSource } = require('typeorm');
-
-
-const appDataSource = new DataSource({
-  type: process.env.DB_CONNECTION,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
-
-appDataSource
-    .initialize()
-    .then(() => {
-        console.log("Data Source has been initialized!");
-    })
-    .catch((err) => {
-        console.error("Error during Data Source initialization:", err);
-    })
+const DataSource  = require('./models/dataSource');
 
 const app = express();
 
@@ -36,6 +17,15 @@ app.get('/ping', function (req, res)  {
 
 const PORT = process.env.PORT 
 
-app.listen(PORT, () => {
+app.listen(PORT,async () => {
+  await DataSource
+  .initialize()
+  .then(() => {
+      console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+      console.error("Error during Data Source initialization:", err);
+  })
+
   console.log(`server listening on port ${PORT}`);
 })
