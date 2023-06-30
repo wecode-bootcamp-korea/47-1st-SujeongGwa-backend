19298c2049e4167fd7e02834a75a7df99b940517
mac <mac@macui-MacBookAir.local>
2023-06-30 14:10:29 +0900

    ADD: cart 폴더 구분 -서동희

--- models/productDao.js
+++ models/productDao.js
@@ -1,1 +1,26 @@
 const dataSource = require("./dataSource");
+
+  const getProductDetails= async function (products.id) {
+       try{
+        const detail=await dataSource.query ( 
+        `
+        SELECT
+        sub_category_id, 
+        name, 
+        surface_type_id, 
+        price, 
+        weight
+        FROM 
+        products
+        WHERE products.id=?
+      `,
+      [products.id]
+      );
+       return detail;
+    }catch(error){
+        error = new Error('DATABASE_CONNECTION_ERROR');
+        error.statusCode = 400;
+        throw error;  
+    };
+
+module.exports = { getProductDetails};
