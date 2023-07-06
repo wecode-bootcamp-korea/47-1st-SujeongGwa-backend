e3a06a091109f27b50f6f8a82cbd3cb2fa0c53f0
mac <mac@macui-MacBookAir.local>
2023-07-06 17:10:49 +0900

    welcomeEmail -서동희

--- services/userService.js
+++ services/userService.js
@@ -60,7 +60,7 @@ const sendEmail = async function (userEmail) {
     from: 'Sujeongwa6@gmail.com',
     to: userEmail,
     subject: 'WELCOME TO SJG',
-    html: '<p><strong>WELCOME TO SJG TILE!</strong></p><p>ENJOY YOUR NEVER-EXPIRING WELCOME GIFT OF <u>10,000,000 WON</u></p><p>YOU CAN USE IT AT ANY TIME.</p><p>SJW타일에 가입하신 것을 환영합니다!!.</p><p>유효기간이 없는 <u>10,000,000포인트</u>를 지급했습니다.</p><p>언제든 사용하세요!</p>',
+    html: '<p><strong>WELCOME TO SJG TILE!</strong></p><p>ENJOY YOUR NEVER-EXPIRING WELCOME GIFT OF <u>10,000,000 WON</u></p><p>YOU CAN USE IT AT ANY TIME.</p><p>SJG타일에 가입하신 것을 환영합니다!!.</p><p>유효기간이 없는 <u>10,000,000포인트</u>를 지급했습니다.</p><p>언제든 사용하세요!</p>',
   };
 
   try {
